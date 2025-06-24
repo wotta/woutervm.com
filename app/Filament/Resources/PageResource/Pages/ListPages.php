c055547bffa43e71d558fcd3eda6390329fdaf92
Wouter (wotta) <wouter.van.marrum@protonmail.com>
2025-06-24 12:55:57 +0200

    Add ListPages resource page

---
+++ app/Filament/Resources/PageResource/Pages/ListPages.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Resources\PageResource\Pages;
+
+use App\Filament\Resources\PageResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListPages extends ListRecords
+{
+    protected static string $resource = PageResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTitle(): string
+    {
+        return 'Pages';
+    }
+}
