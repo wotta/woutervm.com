b3a092faacc06e42673bd919f1e2c65d6635bc94
Wouter (wotta) <wouter.van.marrum@protonmail.com>
2025-06-24 12:56:12 +0200

    Add CreatePage resource page

---
+++ app/Filament/Resources/PageResource/Pages/CreatePage.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Filament\Resources\PageResource\Pages;
+
+use App\Filament\Resources\PageResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePage extends CreateRecord
+{
+    protected static string $resource = PageResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
+    protected function getCreatedNotificationTitle(): ?string
+    {
+        return 'Page created successfully';
+    }
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // Set published_at to now if status is published and no published_at is set
+        if ($data['status'] === 'published' && empty($data['published_at'])) {
+            $data['published_at'] = now();
+        }
+
+        return $data;
+    }
+}
